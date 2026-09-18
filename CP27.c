2a4578dd1b645160d9f9cb5a6944efc075c46fa5
Marojjsk2 <marojjsk@gmail.com>
2026-09-18 11:35:42 +0900

    Add files via upload

---
+++ CP27.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int cur = 2;
+	int is = 0;
+
+	while (cur < 10)
+	{
+		is = 1;
+		while (is < 10)
+		{
+			printf("%d x %d = %d   ", cur, is, cur * is);
+			is++;
+		}
+		printf("\n");
+		cur++;
+	}
+	return 0;
+}
